75b3a0fa12cd8a53554c6f2246495f062b10f512
Arnaud Gelas <arnaudgelas@gmail.com>
2014-06-20 09:56:08 +0200

    Prepare project to be included as ITK remote module

---
+++ LaplacianDeformation.remote.cmake
@@ -0,0 +1,5 @@
+itk_fetch_module(LaplacianDeformation
+  "Laplacian Mesh Deformtation"
+  GIT_REPOSITORY https://github.com/arnaudgelas/LaplacianDeformation.git
+  GIT_TAG master
+  )
---
+++ itk-module.cmake
@@ -0,0 +1,11 @@
+set(DOCUMENTATION "This module contains a class to perform Mesh Laplacian Deformation"
+
+itk_module( LaplacianDeformation
+  DEPENDS
+    ITKQuadEdgeMeshFiltering
+ TEST_DEPENDS
+    ITKTestKernel
+  EXCLUDE_FROM_DEFAULT
+  DESCRIPTION
+    "${DOCUMENTATION}"
+)
